c4594d032f53564610941b881c0e320ec361c8af
Mike Milano <coder1@gmail.com>
2014-08-03 22:26:59 -0700

    added lodash

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,6 +15,7 @@ module.exports = function(grunt) {
         vendor: [
           'bower_components/angular/angular.js',
           'bower_components/angular-route/angular-route.js',
+          'bower_components/lodash/dist/lodash.js',
           'bower_components/angular-bootstrap/ui-bootstrap.js'
         ],
         src: [
